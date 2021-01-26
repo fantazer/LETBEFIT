d2dc9b21295ef8fb436be421c6e540e224d0b736
FANTAZER <kuznecovs45@gmail.com>
2021-01-26 21:18:30 +0300

    add modal markDelivery + start promo

--- app/js/script.js
+++ app/js/script.js
@@ -626,6 +626,65 @@ $(document).ready(function () {
 	});
 	// set mark === end
 
+	// countDown
+	if ($('.timer').length > 0) {
+		function makeTimer() {
+
+			var endTime = new Date("29 April 2018 9:56:00 GMT+01:00");
+			//var endTime = new Date("26/01/2021");
+			endTime = (Date.parse(endTime) / 1000);
+
+			var now = new Date();
+			now = (Date.parse(now) / 1000);
+
+			var timeLeft = endTime - now;
+
+			var days = Math.floor(timeLeft / 86400);
+			var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+			var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600)) / 60);
+			var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+
+			if (hours < "10") {
+				hours = "0" + hours;
+			}
+			if (minutes < "10") {
+				minutes = "0" + minutes;
+			}
+			if (seconds < "10") {
+				seconds = "0" + seconds;
+			}
+
+			$("#days").html(days + "<span>Days</span>");
+			$(".timer__el-hour").html(hours);
+			$(".timer__el-min").html(minutes);
+			$(".timer__el-sec").html(seconds);
+
+		}
+
+		setInterval(function () {
+			makeTimer();
+		}, 1000);
+	}
+
+	// countDown === end
+
+	// paralax
+	if ($('.paralax-el').length > 0) {
+		$(window).scroll(function() {
+
+			$('.paralax-el').each(function(){
+				var depth = - $(window).scrollTop() / $(this).data('depth')*1;
+				//depth = depth || 40
+				$(this).css({
+					"-webkit-transform" : "translate3d(0px, " + depth  + "%, 100px)",
+				});
+			})
+		})
+	}
+	// paralax === end
+
+
+
 	window.condition = {};
 	window.condition.closeModal = closeModal
 	window.condition.initModal = initModal
--- gulpfile.js
+++ gulpfile.js
@@ -193,6 +193,8 @@ gulp.task('watch', () => {
 
 gulp.task('pug', function() {
 		gulp.src(['app/html/*.pug','app/module/**/*.pug',])
+		//gulp.src(['app/html/main.pug','app/module/**/*.pug',])
+		//gulp.src(['app/html/modal.pug','app/module/**/*.pug',])
 		/*gulp.src([
 			'app/html/smi-list.pug',
 			'app/html/smi-item.pug',
@@ -201,7 +203,6 @@ gulp.task('pug', function() {
 			'app/html/contact.pug',
 			'app/module/!**!/!*.pug',
 		])*/
-		//gulp.src(['app/html/modal.pug','app/module/**/*.pug',])
 		//gulp.src(['app/html/lk-*.pug','app/module/**/*.pug',])
 				//.pipe(changed('app/', {extension: '.html'}))
 				//.pipe(cache('pug'))
